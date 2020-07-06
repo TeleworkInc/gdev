cb2feb1b8079eae8173c7db049fd661250d3ec29
Christian Lewis <ctjl96@gmail.com>
2020-07-06 03:50:50 -0500

    BULK AUTOMATIC COMMIT: Mon 06 Jul 2020 03:50:50 AM CDT

---
+++ commands/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const touch = require('touch');
+const path = require('path');
+const chalk = require('chalk');
+
+/**
+ * Define any core logic here.
+ */
+
+const create = (name) => {
+
+    const abs = path.resolve(name);
+    if (!fs.existsSync(abs))
+        fs.mkdirSync(abs),
+            touch(path.resolve(abs, '.gproj')),
+            success(
+                'Created project at:',
+                blue(abs),
+            );
+
+    else error('File or directory already exists.');
+
+}
+
+const debug = () => {
+    console.log('Hello world!');
+}
+
+const dev = () => {
+    console.log('Hello world!');
+}
+
+const dist = () => {
+    console.log('Hello world!');
+}
