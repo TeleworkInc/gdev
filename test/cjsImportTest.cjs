ec1af9050563fab89f0b6673f6e24968a19b58a8
Christian Lewis <ctjl96@gmail.com>
2020-07-23 01:13:48 -0500

    Tests passing, package.json revised

--- test/cjsImportTest.cjs
+++ test/cjsImportTest.cjs
@@ -1,15 +1,32 @@
 const { expect } = require('chai');
-const CJSImport = require('../dev/node.cjs');
-const CJSMinImport = require('../dist/node.min.cjs');
+const cjsNode = require('../dev/node.cjs');
+const cjsNodeMin = require('../dist/node.min.cjs');
+
+const cjsCli = require('../dev/cli.cjs');
+const cjsCliMin = require('../dist/cli.min.cjs');
+
+const thisPackage = require('..');
 
 describe('CJS require()', function() {
-  it('should successfuly import the uncompiled module [dev/node.cjs]',
+  it('should import this npm package', () => {
+    expect(thisPackage.callCompiler).to.be.a('function');
+  });
+
+  it('should import the uncompiled module [dev/node.cjs]',
       function() {
-        expect(CJSImport.callCompiler).to.be.a('function');
+        expect(cjsNode.callCompiler).to.be.a('function');
       });
 
-  it('should successfuly import the compiled module [dist/node.min.cjs]',
+  it('should import the compiled module [dist/node.min.cjs]',
       function() {
-        expect(CJSMinImport.callCompiler).to.be.a('function');
+        expect(cjsNodeMin.callCompiler).to.be.a('function');
       });
+
+  it('should not fail for uncompiled CLI bundle [dev/cli.cjs]', function() {
+    expect(() => cjsCli).to.not.throw();
+  });
+
+  it('should not fail for compiled CLI bundle [dist/cli.min.cjs]', function() {
+    expect(() => cjsCliMin).to.not.throw();
+  });
 });
--- test/esmImportTest.js
+++ test/esmImportTest.js
@@ -1,17 +1,34 @@
+import * as thisPackage from 'gdev';
 import chai from 'chai';
-import * as NodeModule from '../dev/node.mjs';
-import * as NodeMinModule from '../dist/node.min.mjs';
+
+import * as esmCli from '../dev/cli.mjs';
+import * as cjsCliMin from '../dist/cli.min.mjs';
+import * as esmNode from '../dev/node.mjs';
+import * as esmNodeMin from '../dist/node.min.mjs';
 
 const { expect } = chai;
 
 describe('ESM import', function() {
+  it('should successfuly import this package',
+      function() {
+        expect(thisPackage.create).to.be.a('function');
+      });
+
+  it('should not fail for uncompiled ESM [dev/cli.mjs]', function() {
+    expect(() => esmCli).to.not.throw();
+  });
+
+  it('should not fail for compiled ESM [dist/cli.min.mjs]', function() {
+    expect(() => cjsCliMin).to.not.throw();
+  });
+
   it('should successfuly import the uncompiled module [dev/node.mjs]',
       function() {
-        expect(NodeModule.create).to.be.a('function');
+        expect(esmNode.create).to.be.a('function');
       });
 
   it('should successfuly import the compiled module [dist/node.min.mjs]',
       function() {
-        expect(NodeMinModule.create).to.be.a('function');
+        expect(esmNodeMin.create).to.be.a('function');
       });
 });
