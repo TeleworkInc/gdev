dd9d68df43310fdba852bdf84e4ecc9dabcdd2b4
Christian Lewis <ctjl96@gmail.com>
2020-08-15 13:55:32 -0500

    Committing before upgrade

---
+++ testImport.cjs
@@ -0,0 +1,2 @@
+const { TEST_STRING } = require('./dist/universal.cjs');
+console.log({ TEST_STRING });
---
+++ testImport.mjs
@@ -0,0 +1,3 @@
+import node from './dist/node.mjs';
+import { build } from './dist/node.mjs';
+console.log(build);
